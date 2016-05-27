6ba22218237d39076c8a3f5d3fe572ca2f07fe09
James Kite <james.kitejw@btinternet.com>
2016-05-27 14:15:37 +0100

    Commenting some Board.h member declarations

--- Aspects/Aspects/Board.h
+++ Aspects/Aspects/Board.h
@@ -15,9 +15,14 @@ public:
 	Board();
 	~Board();
 private:
+
+	// The Player object corresponding to Player 1
 	Player p1;
+
+	// The Player object corresponding to Player 2
 	Player p2;
+
+	// The Tile array holding all the tiles on the board
 	Tile board[80][40];
-	// making a change to the file
 };
 
