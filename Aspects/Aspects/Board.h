1b81fd169fe5164481b63758c67c82e1372fae5a
James Kite <james.kitejw@btinternet.com>
2016-05-27 13:37:50 +0100

    Adding VS project
    Placing VS project in repository!

---
+++ Aspects/Aspects/Board.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include<vector>
+
+#include "Tile.h"
+#include "Player.h"
+
+using namespace std;
+
+class Board
+{
+public:
+	Board();
+	~Board();
+private:
+	Player p1;
+	Player p2;
+	Tile board[80][40];
+};
+
