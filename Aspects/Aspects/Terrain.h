4c80249edc9115ef7406fec6038d93414565a146
Matthew Kite <Matthew.kite@btinternet.com>
2016-05-31 17:37:49 +0100

    I added a new class - James

---
+++ Aspects/Aspects/Terrain.h
@@ -0,0 +1,17 @@
+#ifndef TERRAIN_H
+#define TERRAIN_H
+
+#include<string>
+
+using namepsace std;
+
+static class Terrain
+{
+    public:
+        static string getRandomType();
+    protected:
+    private:
+        string typeArr[];
+};
+
+#endif // TERRAIN_H
